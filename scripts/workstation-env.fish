b7cd555c5b549aa0de7f9d8a40dbcf303f9d0ce2
nethrose <6612612+nethrose@users.noreply.github.com>
2026-06-28 11:04:18 -0700

    Note Twingate remote kubectl is separate from LAN KUBECONFIG.

--- scripts/workstation-env.fish
+++ scripts/workstation-env.fish
@@ -2,8 +2,9 @@
 # One-time: add to ~/.config/fish/config.fish:
 #   source ~/Documents/GitHub/k3sraspbian/scripts/workstation-env.fish
 #
-# Or set globally without sourcing this file:
-#   set -gx KUBECONFIG $HOME/.kube/k3s-rbps.yaml
+# Sets LAN admin kubeconfig (full cluster access). For Twingate remote kubectl, instead:
+#   set -e KUBECONFIG
+#   kubectl config use-context twingate-k3s-rbps-api
 
 if not set -q KUBECONFIG
     set -gx KUBECONFIG $HOME/.kube/k3s-rbps.yaml
